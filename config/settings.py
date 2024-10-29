efd5da9de7295650e06fd7d9d537c5cb33ded11f
asadbek000002 <asadbektuygunov9@gmail.com>
2024-10-29 20:40:46 +0500

    proba models delete

--- config/settings.py
+++ config/settings.py
@@ -116,24 +116,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'bbc',
-#         'USER': 'bbc_user',
-#         'PASSWORD': 'bbc_password',
-#         'HOST': 'bbc_db',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bbc',
+        'USER': 'bbc_user',
+        'PASSWORD': 'bbc_password',
+        'HOST': 'bbc_db',
+        'PORT': '5432',
+    }
+}
 
 
 
--- news/models.py
+++ news/models.py
@@ -15,20 +15,3 @@ class News(models.Model):
     class Meta:
         verbose_name = 'News'
         verbose_name_plural = 'News'
-
-
-
-class Proba(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    image = models.FileField(upload_to='media/news/image/', blank=True, null=True)
-    order = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Proba'
-        verbose_name_plural = 'Probas'
